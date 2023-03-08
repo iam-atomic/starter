da10f89076583ec36b4f83532128b19a40889637
iam-atomic <127174926+iam-atomic@users.noreply.github.com>
2023-03-08 12:31:38 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: Hello World
+* Plugin URI: https://www.your-site.com/
+* Description: Quisque velit nisi, pretium ut lacinia in, elementum id enim. Nulla porttitor accumsan tincidunt.
+* Version: 0.1
+* Author: your-name
+* Author URI: https://www.your-site.com/
+**/
